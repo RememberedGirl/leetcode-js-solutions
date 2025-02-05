de8d0009ee029abbfb2e0a24fe0a5e063aabc699
quuu131 <quuu131@gmail.com>
2025-02-05 15:47:37 +0300

    - 58

---
+++ Easy/58. Length of Last Word/solution.js
@@ -0,0 +1,36 @@
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+
+    const words = s.trim().split(' ');
+    return words[words.length - 1].length;
+
+};
+
+
+console.log()
+if (typeof document !== 'undefined') {
+    const output = document.getElementById('output');
+
+
+    const displayResult = (s) => {
+        const div = document.createElement('div');
+        // `n: ${n}, fn: ${fn.toString()} -> ${JSON.stringify(result)}`
+        // Array.from(
+        //     { length: n },
+        //     (_, index) => index
+        // );
+
+        const ret = lengthOfLastWord(s)
+        div.textContent = `s: ${s}: ${ret}`
+        output.appendChild(div);
+    };
+
+    displayResult( "luffy is still joyboy");
+    displayResult( "   fly me   to   the moon  ");
+    displayResult( "luffy is still joyboy");
+
+}
