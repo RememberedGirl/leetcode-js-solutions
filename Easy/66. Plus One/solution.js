1990af3b18034b272c3b421568f3475a57ba85ef
quuu131 <quuu131@gmail.com>
2025-02-05 22:55:46 +0300

    - 67

--- Easy/66. Plus One/solution.js
+++ Easy/66. Plus One/solution.js
@@ -1,19 +1,24 @@
-
 /**
- * @param {string} s
- * @return {number}
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
  */
-var plusOne = function(digits) {
-    for (let i = digits.length - 1; i >= 0; i--) {
+var addBinary = function(a, b) {
+    // return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2)
 
-        if (digits[i] < 9) {
-            digits[i]++;
-            return digits;
-        }
+    let ret = "";
+    let sum = 0;
+    let i = a.length - 1, j = b.length - 1;
 
-        digits[i] = 0;
+    while (i >= 0 || j >= 0 || sum) {
+        if (i >= 0) sum += Number(a[i--]);
+        if (j >= 0) sum += Number(b[j--]);
+
+        ret = (sum % 2) + ret;
+        sum = (sum / 2) >= 1;
     }
-    return [1, ...digits];
+
+    return ret;
 };
 
 
@@ -22,7 +27,7 @@ if (typeof document !== 'undefined') {
     const output = document.getElementById('output');
 
 
-    const displayResult = (digits) => {
+    const displayResult = (a, b) => {
         const div = document.createElement('div');
         // `n: ${n}, fn: ${fn.toString()} -> ${JSON.stringify(result)}`
         // Array.from(
@@ -30,14 +35,16 @@ if (typeof document !== 'undefined') {
         //     (_, index) => index
         // );
 
-        const digits_ = JSON.stringify(digits)
-        const ret = plusOne(digits)
-        div.textContent = `digits: ${digits_} ->${ret}`
+
+        const ret = addBinary(a, b)
+        div.textContent = `a: ${a}, b: ${b} -> ${ret}`
         output.appendChild(div);
     };
 
-    displayResult( [1,2,3]);
-    displayResult( [9]);
-    displayResult( [4,3,2,9]);
+    displayResult("11", "1");
+    displayResult("11","0");
+    displayResult("1010", "1011");
+    displayResult("1111", "1111");
+    displayResult("11110000000000000", "1111");
 
 }
--- Easy/67. Add Binary/README.md
+++ Easy/67. Add Binary/README.md
@@ -1,8 +1,13 @@
-# 67. Add Binary
+# 66. Plus One
 
 ## Problem Statement
 
-Given two binary strings `a` and `b`, return their sum as a binary string.
+You are given a large integer represented as an integer array `digits`,
+where each `digits[i]` is the `i^th` digit of the integer.
+The digits are ordered from most significant to least significant in left-to-right order.
+The large integer does not contain any leading `0`'s.
+
+Increment the large integer by one and return the resulting array of digits.
 
 ### Examples    
 ```javascript
@@ -26,7 +31,7 @@ Given two binary strings `a` and `b`, return their sum as a binary string.
 
 
 ### Problem Link
-[LeetCode Problem](https://leetcode.com/problems/add-binary)
+[LeetCode Problem](https://leetcode.com/problems/plus-one)
 
 ---
 
